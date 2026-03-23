837fadb60c90f1e94037f5aa79ccf22a72aaf0be
Alexis <alexis@arch.localdomain>
2026-03-23 15:05:36 +0100

    Added login page prototype

--- login/views.py
+++ login/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def login_prompt(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/dashboard/")
+        else:
+            return render(
+                request, "login/login.html", {"error": "Falsche Zugangsdaten"}
+            )
+    return render(request, "login/login.html")
