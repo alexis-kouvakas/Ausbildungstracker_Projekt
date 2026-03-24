0a6bd8739cf2ad4cd8c73ff291ecd82c77422328
Alexis <alexios.kouvakas@proton.me>
2026-03-24 14:48:45 +0100

    fixed dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+import csv
+import os
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.contrib.auth.decorators import login_required  # 1. Importieren
+from django.shortcuts import render
 
-
-# Create your views here.
 @login_required
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "dashboard/index.html")
+def index(request: HttpRequest, liste='BS') -> HttpResponse:
+    filename = f'Kursliste_{liste}.csv'
+    file_path = os.path.join(os.path.dirname(__file__), filename)
+    
+    kurse_daten = []
+    
+    try:
+        with open(file_path, mode='r', encoding='utf-8') as csvfile:
+            # DictReader nutzt die Kopfzeile (Kursname, Status) als Keys
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                kurse_daten.append(row)
+    except FileNotFoundError:
+        pass
+
+    context = {
+        'liste': liste,
+        'kurse': kurse_daten,
+        'status_optionen': ['offen', 'in Bearbeitung', 'fertig']
+    }
+    
+    return render(request, "dashboard/index.html", context)
