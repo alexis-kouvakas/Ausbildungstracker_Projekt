987a0321fec6117681d8387ac31ed0483d66465e
Alexis <alexis@arch.localdomain>
2026-03-24 14:01:55 +0100

    Login made necessary for accessing dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required  # 1. Importieren
 
 
 # Create your views here.
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "dashboard/index.html")
