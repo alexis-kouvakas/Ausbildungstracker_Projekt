c5e48c6383a293ba43f7bd79c6dd92f29989a2c9
Leon <walter@server.tracker>
2026-03-24 11:40:53 +0100

    erstellung des superuser, config views, url, models und settings in login

--- tracker/settings.py
+++ tracker/settings.py
@@ -84,9 +84,10 @@ DATABASES = {
     }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'login.Azubi'
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
